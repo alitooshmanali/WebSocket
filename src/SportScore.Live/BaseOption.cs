abd0cf69704de6cd6d2c835fff0c869c5793bde6
Ali Tooshmalani <alitooshmalani93@gmail.com>
2023-07-05 20:08:32 +0330

    commit some things

--- src/SportScore.Live/BaseOption.cs
+++ src/SportScore.Live/BaseOption.cs
@@ -4,6 +4,6 @@
     {
         public static string BaseUrl = "wss://tipsscore.com:2083/app/7UXH2sNFqpVAO6FebyTKpujgfy8BUnM?protocol=7&client=js&version=5.0.3&flash=false";
 
-        public static string AliveMessage = "{\"event\":\"pusher:ping\",\"data\":{}}";
+        public static string AliveMessage = "{\"event\":\"pusher:ping\",\"data\":{}}";  
     }
 }
